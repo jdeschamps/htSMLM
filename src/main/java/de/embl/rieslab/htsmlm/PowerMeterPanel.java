1f87cbce8a03e187d5f303f66bd63a27887d95bb
Joran Deschamps <6367888+jdeschamps@users.noreply.github.com>
2025-02-19 14:26:36 +0100

    Update PowerMeterPanel.java
    Reverse commit

--- src/main/java/de/embl/rieslab/htsmlm/PowerMeterPanel.java
+++ src/main/java/de/embl/rieslab/htsmlm/PowerMeterPanel.java
@@ -192,10 +192,6 @@ public class PowerMeterPanel extends ConfigurablePanel{
 				DefaultComboBoxModel<String> model = new DefaultComboBoxModel<String>(vals);
 				comboBox_.removeAllItems();
 				comboBox_.setModel(model);
-
-				if(vals.length > 0){
-					comboBox_.setSelectedIndex(0);
-				}
 			} catch (UnknownUIParameterException e) {
 				e.printStackTrace();
 			}
